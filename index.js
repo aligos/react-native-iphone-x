a1b92ddc5b8b1a4d353466f7aaaed20cbf973301
Rahmat Aligos <rahmat@clloh.com>
2018-02-06 13:43:38 +0700

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { Dimensions, Platform } from 'react-native';
+
 const { height } = Dimensions.get('window');
+const IS_IOS = Platform.OS === 'ios';
 
 export default function isiPhoneX() {
-  return Platform.OS === 'ios' && (height === 2436 || height === 812) ? true : false;
+  return IS_IOS && (height === 2436 || height === 812) ? true : false;
 }
